14e8835b5e24228c57b26e4521301da1ac637919
SANJAY DUTT <duttmagic5@gmail.com>
2026-04-20 22:02:44 +0530

    somes fix

--- Routes/PostRoutes.js
+++ Routes/PostRoutes.js
@@ -1,10 +1,22 @@
 let express = require('express');
+const { default: Otps } = require('../Schema/Otp');
 let Router = express.Router();
 
 
 Router.post('/login', (req,res)=>{
-   console.log('req come ')
-   res.send('ok');
+    let phone = req.body.phone;
+    let TempAuth = new Otps({
+      phone: phone
+    })
+    TempAuth.save().then(()=>{
+      res.status(200).json({
+         phone: phone
+      })
+    }).catch(()=>{
+      res.status(501).json({
+         reason : 'Unable To Send OTP'
+      })
+    })
 })
 
 
---
+++ Schema/Otp.js
@@ -0,0 +1,12 @@
+let mongoose = require('mongoose')
+
+let Otp = mongoose.Schema({
+    phone: {type:Number, required:true},
+    otp:{type:Number, required:true, default:Math.floor(100000 + Math.random()*100000)
+ }
+
+})
+
+
+let Otps = mongoose.model('Otp', Otp, 'Otp');
+export default Otps;
